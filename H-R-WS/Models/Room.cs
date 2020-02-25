08ace0aba6036da3d44ad422a974ca332d0bd02a
DDDigital <digitaldrugdealer2@gmail.com>
2020-02-25 11:31:24 +0200

    Room model

---
+++ H-R-WS/Models/Room.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H_R_WS.Models
+{
+    public class Room
+    {
+        public Guid ID { get; set; }
+        public int Number { get; set; }
+        public Guid RoomTypeID { get; set; }
+        public virtual RoomType RoomType { get; set; }
+        public decimal Price { get; set; }
+        public bool Available { get; set; }
+        public string Description { get; set; }
+        public int MaximumGuests { get; set; }
+        public virtual List<Feature> Features { get; set; }
+        public virtual List<Image> RoomImages { get; set; }
+        public virtual List<Review> Reviews { get; set; }
+        public virtual List<Booking> Bookings { get; set; }
+    }
+}
